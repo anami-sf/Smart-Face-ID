8a24af5cf713c6c4b8b5928362d80077a6328997
Ana <anami823@gmail.com>
2019-01-27 16:35:42 -0800

    Styled  text blox

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const ImageLinkForm = () => {
     return(
-        <div className = 'pa4'>
+        <div className = 'pa4 '>
             <p>{'Smart Face ID will detect faces in your pictures.'}<br/>
                 {'Enter a link to your picture below.'}
             </p>
-            <input className = 'buttons' type = 'text' ></input>
-            <button style = {{color: 'white', backgroundColor: 'rgb(49, 67,80)'}}>Detect</button>
+            <div className = 'center pa4 bg-blue w-30 '>
+                <input className = 'buttons' type = 'text' ></input>
+                <button style = {{color: 'white', backgroundColor: 'rgb(49, 67,80)'}}>Detect</button>
+            </div>
         </div>
     )
 }
